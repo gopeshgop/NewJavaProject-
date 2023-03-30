90db3073c35ea24ca814c5fa3f953fe4fe290ff1
gopeshgop <gopeshgop@gmail.com>
2023-03-30 19:05:29 -0300

    dowhile commit

---
+++ src/tableOfTwo/PrintTableDoWhile.java
@@ -0,0 +1,14 @@
+package tableOfTwo;
+
+public class PrintTableDoWhile {
+	int number;
+	
+	void printMultiples() {
+		//int finalNumber = 10;
+		do {
+			System.out.println(number);
+			number+=2;
+		}while(number<=10);
+	}
+
+}
