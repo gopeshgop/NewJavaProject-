e8036de94d9c52582ab0fa5ee81490d86d4d8dc0
gopeshgop <gopeshgop@gmail.com>
2023-03-30 18:44:44 -0300

    Todays commit

---
+++ src/tableOfTwo/MainPrint.java
@@ -0,0 +1,12 @@
+package tableOfTwo;
+
+public class MainPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintTableOfTwo printTwo = new PrintTableOfTwo();
+		printTwo.printMultiples();
+
+	}
+
+}
---
+++ src/tableOfTwo/PrintTableOfTwo.java
@@ -0,0 +1,13 @@
+package tableOfTwo;
+public class PrintTableOfTwo {
+
+	int number;
+	
+	void printMultiples() {
+		//int finalNumber = 10;
+		while(number<=10) {
+			System.out.println(number);
+			number+=2;
+		}
+	}
+}
