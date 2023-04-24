6bd83c55717f6f98000e25bb1f926efad6abde3d
gopeshgop <gopeshgop@gmail.com>
2023-04-24 18:19:58 -0300

    Assignment Game

--- src/assignmntApril21Game/NameSelectGameMain.java
+++ src/assignmntApril21Game/NameSelectGameMain.java
@@ -4,33 +4,27 @@ import java.util.Arrays;
 public class NameSelectGameMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		NameSelectGame nameSelectGame = new NameSelectGame();
 		char toCheck; int wrongInputCount=0;
-		//System.out.println("Enter your name to start the game");
-		//String getNameOfPlayer = sc.next();
-		
 		String randomName = nameSelectGame.randomSelect();
-		
-		System.out.println(randomName);
 		
 		char[]charArrayRandomName = nameSelectGame.convertrandomNameToArray(randomName);
 		
 		char[] charArrayRandomNameCopy=nameSelectGame.initializeCopyArray(charArrayRandomName);
-		
-		
+		System.out.println("Here is a name to guess");
+		System.out.println(charArrayRandomNameCopy);
 		while(wrongInputCount<5) {
-			System.out.println();
-			System.out.println("Enter a letter to check ");
+			
+			System.out.println("Enter a guess letter ");
+			System.out.println("You have "+(5-wrongInputCount)+" chance ");
 			toCheck = sc.next().charAt(0);
 			
 			char[]testCharArrayRansomNameCopy = Arrays.copyOf(charArrayRandomNameCopy, charArrayRandomNameCopy.length);
 			
 			charArrayRandomNameCopy=
 					nameSelectGame.checkForTheLetterInName(charArrayRandomName, charArrayRandomNameCopy, toCheck);
-			
-			
 			int checkArraysEqualOrNot=Arrays.compare(testCharArrayRansomNameCopy,charArrayRandomNameCopy);
 			int breakWhileLoop=Arrays.compare(charArrayRandomNameCopy,charArrayRandomName);
 			
@@ -41,6 +35,7 @@ public class NameSelectGameMain {
 			for(int i=0;i<charArrayRandomName.length;i++) {
 				System.out.print(charArrayRandomNameCopy[i]);
 			}
+			
 		
 			if(breakWhileLoop==0) {
 				
@@ -50,13 +45,8 @@ public class NameSelectGameMain {
 			
 		}
 		if(wrongInputCount==5) {
-			System.out.println("You have entered wrong input 5 times...You lost ..Game over");
-		}
-		
-		
-		
-		
-		
+			System.out.println("\nYou have entered wrong input 5 times...You lost ..Game over");
+		}	
 		
 	}
 
